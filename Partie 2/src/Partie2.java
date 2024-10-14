c1b9fe848c8fafe865d509f6a087fba4a345e938
melanie552 <melanie.mathourais@gmail.com>
2024-10-14 16:17:11 +0200

    message

--- Partie 2/src/Partie2.java
+++ Partie 2/src/Partie2.java
@@ -1,1 +1,19 @@
-
+class Partie2{
+	void principal(){
+		
+	}
+	
+	int [] binaire(int nb){
+		int [] bin = {16,8,4,2,1};
+		int []res = new int [5];
+		
+		for (int i = 0; i < 5 ; i ++){
+			if (nb >= bin[i] ){
+				nb -= bin[i];
+				res[i] = 1;
+			}
+		}
+		
+		return res;
+	}
+}
