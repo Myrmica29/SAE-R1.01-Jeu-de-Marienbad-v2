0233589c6f3f43f3ba4818e5c407a1b580fb8df9
melanie552 <melanie.mathourais@gmail.com>
2024-10-14 17:26:59 +0200

    message

--- Partie 1/src/Partie1.java
+++ Partie 1/src/Partie1.java
@@ -18,11 +18,6 @@ class Partie1{
 		int nbAll ; // Variable utilisée dans la boucle pour stocker le nombre d'allumettes à enlever
 		
 		
-		// Saisie du nom du joueur qui joue en 1er
-		joueur1 = nomJoueur(1, "");
-		
-		// Saisie du nom du joueur qui joue en 2ème
-		joueur2 =nomJoueur(2, joueur1);
 		
 		
 		// Saisie du nombre de lignes
@@ -35,6 +30,12 @@ class Partie1{
 		
 		turn = 1;
 		run = true;
+		
+		// Saisie du nom du joueur qui joue en 1er
+		joueur1 = nomJoueur(1, "");
+		
+		// Saisie du nom du joueur qui joue en 2ème
+		joueur2 =nomJoueur(2, joueur1);
 		
 		
 		while (run){
@@ -75,21 +76,7 @@ class Partie1{
 		}
 	}
 	
-	/**
-	 * Affiche tous les tests de fonction
-	 */
-	void test(){
-		System.out.println("**** TEST ****");
-		
-		testEgalString();
-		testGenerateSticks();
-		testUpdateSticks();
-		testContinueGame();
-		
-		System.out.println();
-		System.out.println("**************");
-		System.out.println();
-	}
+	
 	
 	
 	/**
@@ -151,6 +138,106 @@ class Partie1{
 		return equal;
 	}
 	
+	
+	
+	
+	/**
+	 * cree un tableau contenant le nombre initial d'allumette en debut de partie
+	 * @param n nombre de ligne
+	 * @return stick tableau, des lignes d'allumette
+	 */
+	int[] generateSticks(int n){
+		int[] stick = new int[n];
+		for ( int i = 0; i <n ; i ++){
+			stick[i] = 1 + i*2;
+		}
+		return stick;
+	}
+	
+	
+	/**
+	 * Affiche les allumettes
+	 * @param sticks tableau contenant le nombre d'allumettes par lignes
+	 */
+	void displaySticks(int[] sticks){
+		System.out.println();
+		for (int i=0; i<sticks.length;i++){
+			System.out.print(i+" : ");
+			for (int j=0; j<sticks[i];j++){
+				System.out.print("|");
+			}
+			System.out.println(" (" +sticks[i]+ ")");
+		}
+		System.out.println();
+	}
+	
+	/**
+	 * actualise le tableau des allumetttes
+	 * @param sticks tableau d'entiers contenant le nombre d'allumettes par ligne
+	 * @param a ligne a modifié
+	 * @param b nombre d'allumettes à retirer
+	 */
+	void updateSticks(int[] sticks, int a, int b){
+		sticks[a] = sticks[a] - b;
+	}
+	
+	
+	
+	/**
+	 * Test si c'est la fin du jeu en regardant si il n'y a plus aucune allumettes sur aucune lignes
+	 * @param sticks tableau d'entiers contenant le nombre d'allumettes par ligne
+	 * @return true si ce n'est pas la fin du jeu
+	 */
+	boolean continueGame(int[] sticks){
+		boolean res = false;
+		for (int i=0; i<sticks.length; i++){
+			if (sticks[i]>0){
+				res = true;
+			}
+		}
+		return res;
+	}
+	
+	
+	
+	
+	
+	/**
+	 * Affiche un tableau d'entiers
+	 * @param t tableau d'entiers
+	 * @return une chaine de caractère qui représente le tableau t
+	 */
+	String displayTab(int[] t){
+		String res = "{";
+		if (t.length > 0){
+			for (int i=0; i<t.length-1; i++){
+				res += t[i] + ",";
+			}
+			res += t[t.length-1];
+		}
+		res += "}";
+		return res;
+	}
+	
+	
+	//fonction test
+	/**
+	 * Affiche tous les tests de fonction
+	 */
+	void test(){
+		System.out.println("**** TEST ****");
+		
+		testEgalString();
+		testGenerateSticks();
+		testUpdateSticks();
+		testContinueGame();
+		
+		System.out.println();
+		System.out.println("**************");
+		System.out.println();
+	}
+	
+	
 	/**
 	 * Test la méthode egalString()
 	 */
@@ -182,20 +269,6 @@ class Partie1{
 		}
 	}
 	
-	
-	/**
-	 * cree un tableau contenant le nombre initial d'allumette en debut de partie
-	 * @param n nombre de ligne
-	 * @return stick tableau, des lignes d'allumette
-	 */
-	int[] generateSticks(int n){
-		int[] stick = new int[n];
-		for ( int i = 0; i <n ; i ++){
-			stick[i] = 1 + i*2;
-		}
-		return stick;
-	}
-	
 	/**
 	 * Test la méthode generateSticks()
 	 */
@@ -224,33 +297,6 @@ class Partie1{
 		} else {
 			System.err.println("ERREUR");
 		}
-	}
-		
-	
-	/**
-	 * Affiche les allumettes
-	 * @param sticks tableau contenant le nombre d'allumettes par lignes
-	 */
-	void displaySticks(int[] sticks){
-		System.out.println();
-		for (int i=0; i<sticks.length;i++){
-			System.out.print(i+" : ");
-			for (int j=0; j<sticks[i];j++){
-				System.out.print("|");
-			}
-			System.out.println(" (" +sticks[i]+ ")");
-		}
-		System.out.println();
-	}
-	
-	/**
-	 * actualise le tableau des allumetttes
-	 * @param sticks tableau d'entiers contenant le nombre d'allumettes par ligne
-	 * @param a ligne a modifié
-	 * @param b nombre d'allumettes à retirer
-	 */
-	void updateSticks(int[] sticks, int a, int b){
-		sticks[a] = sticks[a] - b;
 	}
 	
 	/**
@@ -284,18 +330,54 @@ class Partie1{
 	}
 	
 	/**
-	 * Test si c'est la fin du jeu en regardant si il n'y a plus aucune allumettes sur aucune lignes
-	 * @param sticks tableau d'entiers contenant le nombre d'allumettes par ligne
-	 * @return true si ce n'est pas la fin du jeu
+	 * verifie si deux tableaux sont identiques
+	 * @param t1 premier tableau d'entier
+	 * @param t2 deuxieme tableau d'entier
+	 * @return vrai ssi les deux tableaux sont identiques
 	 */
-	boolean continueGame(int[] sticks){
-		boolean res = false;
-		for (int i=0; i<sticks.length; i++){
-			if (sticks[i]>0){
-				res = true;
+	boolean tabIden( int [] t1, int [] t2){
+		boolean res = true ;
+		if (t1.length != t2.length ){
+			res = false ;
+		}else{
+			for (int i = 0; i < t1.length; i++ ){
+				if (t1[i] != t2[i]){
+					res = false ;
+				}
 			}
 		}
-		return res;
+		return res; 
+	}
+	
+	/**
+	 * Test la méthode tabIden()
+	 */
+	void testTabIden(){
+		System.out.println();
+		System.out.println("*** testUpdateSticks() ***");
+		
+		testCasTabIden(new int [] {0,2,4,2}, new int [] {0,2,4,2}, true);
+		testCasTabIden(new int [] {1},new int [] {1}, true);
+		testCasTabIden(new int [] {1,3,5,7,9}, new int [] {1,3,5,7,0}, false);
+	}
+	
+	/**
+	 * Teste un appel de updateSticks()
+	 * @param tab1 premier tableau d'entiers
+	 * @param tab2 deuxieme tableau d'entiers
+	 * @param result résultat attendu
+	 */
+	void testCasTabIden(int[] tab1, int [] tab2,boolean result){
+		//Affichage
+		System.out.print("tabIden(" + displayTab(tab1) + ", " +displayTab(tab2)+ ")\t= " + result + "\t : ");
+		//Appel
+		boolean res = tabIden(tab1, tab2) ;
+		//Vérification
+		if (res == result){
+			System.out.println("OK");
+		} else {
+			System.err.println("ERREUR");
+		}
 	}
 	
 	/**
@@ -328,42 +410,4 @@ class Partie1{
 			System.err.println("ERREUR");
 		}
 	}
-	
-	/**
-	 * verifie si deux tableaux sont identiques
-	 * @param t1 premier tableau d'entier
-	 * @param t2 deuxieme tableau d'entier
-	 * @return vrai ssi les deux tableaux sont identiques
-	 */
-	boolean tabIden( int [] t1, int [] t2){
-		boolean res = true ;
-		if (t1.length != t2.length ){
-			res = false ;
-		}else{
-			for (int i = 0; i < t1.length; i++ ){
-				if (t1[i] != t2[i]){
-					res = false ;
-				}
-			}
-		}
-		return res; 
-	}
-	
-	/**
-	 * Affiche un tableau d'entiers
-	 * @param t tableau d'entiers
-	 * @return une chaine de caractère qui représente le tableau t
-	 */
-	String displayTab(int[] t){
-		String res = "{";
-		if (t.length > 0){
-			for (int i=0; i<t.length-1; i++){
-				res += t[i] + ",";
-			}
-			res += t[t.length-1];
-		}
-		res += "}";
-		return res;
-	}
-	
 }
